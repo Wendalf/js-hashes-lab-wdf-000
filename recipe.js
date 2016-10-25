cf7c171750b1a291fa97b547905250f84a8ca1a1
Wendy Xu <guangw.xu@gmail.com>
2016-10-24 22:49:48 -0400

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function addIngredient(recipe, ingredientName, amouts){
+  recipe[ingredientName] = amouts;
+  return recipe;
+}
+
+
+function removeIngredient(recipe, ingredientName){
+  delete recipe[ingredientName];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredientName, amouts){
+  recipe[ingredientName] = amouts;
+  return recipe;
+}
+
+function readRecipe(recipe){
+  for(var ingredientName in recipe){
+    console.log("this recipe calls for " + recipe[ingredientName] + " of " + ingredientName);
+  }
+}
